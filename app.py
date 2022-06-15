8cb3fccca86cd6cdea02468431c504e9cdd752eb
Jayanth G <40367036+jayanthgopal@users.noreply.github.com>
2022-06-15 20:50:07 +0100

    Button/Spotify lib

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 st.set_page_config(page_title="Spotify Weekly",layout="wide")
 st.title("Spotify Weekly")
+st.button("label")
