adf28a938bea36524fd50de70b0f80e2ed55a0b3
Jayanth G <40367036+jayanthgopal@users.noreply.github.com>
2022-06-15 21:31:48 +0100

    Table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
 
 CLIENT_ID = "1f87728885ec42db8e6e2bc953c8f702"
 CLIENT_SECRET = "6303b01284414894acd0dff37f3ed3a5"
@@ -11,3 +12,38 @@ st.title("Spotify Weekly")
 #Authentication - without user
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+table_data = {}
+
+index = 0
+df = pd.DataFrame(columns =  ["Track", "Artist", "Genre"])
+
+for track in sp.playlist_tracks(playlist_URI)["items"]:
+    #URI
+    track_uri = track["track"]["uri"]
+    
+    #Track name
+    track_name = track["track"]["name"]
+    
+    #Main Artist
+    artist_uri = track["track"]["artists"][0]["uri"]
+    artist_info = sp.artist(artist_uri)
+    
+    #Name, popularity, genre
+    artist_name = track["track"]["artists"][0]["name"]
+    artist_pop = artist_info["popularity"]
+    artist_genres = artist_info["genres"]
+    
+    #Album
+    album = track["track"]["album"]["name"]
+    
+    #Popularity of the track
+    track_pop = track["track"]["popularity"]
+    df.loc[index] = [track_name, artist_name, artist_genres]
+    index = index + 1
+
+st.table(data=df)    
